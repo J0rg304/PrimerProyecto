8261427a50fe3b63e5c45428669e46320188fd81
1daw <jorge.cabrera@educa.madrid.org>
2022-09-21 19:47:00 +0200

    Ejemplos lectura por teclado

---
+++ src/Ejercicios/Ejercicio17.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class Ejercicio17 {
+    public static void main(String[] args) {
+      //17. Dadas cuatro variables, asigna valores a estas variables. La variable one debería
+        //ser 1, two debería ser 2, three debería ser 3 y four debería ser 4.
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+
+        System.out.println(String.format("%d %d %d %d", one, two, three, four));
+    }
+}
