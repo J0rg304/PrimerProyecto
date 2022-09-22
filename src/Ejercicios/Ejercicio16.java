5948f89455dfaf9892aadabe445c5a76099de41e
1daw <jorge.cabrera@educa.madrid.org>
2022-09-22 18:54:24 +0200

    Ejercicios

--- src/Ejercicios/Ejercicio16.java
+++ src/Ejercicios/Ejercicio16.java
@@ -3,7 +3,7 @@ package Ejercicios;
 public class Ejercicio16 {
     public static void main(String[] args) {
         //16. Haz un programa que declare una variable de tipo int , le asigne el valor 123456
-        //y que a continuación, lo muestre por pantalla.
+        //y que a continuación, lo muestre por pantall
 
         int a = 123456;
         System.out.println(a);
